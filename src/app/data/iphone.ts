147b2dbc1c86fa8d5d0b37c652ef738c248e227c
Youssefragab26 <youssef26ragab@gmail.com>
2025-09-02 21:49:07 +0300

    data update

--- src/app/data/iphone.ts
+++ src/app/data/iphone.ts
@@ -4,4 +4,8 @@ export const iphones = [
   { id: 2, name: "iPhone 16", price: 55500, image: "/images/iphone16.jpeg", href: "/iphone/iphone16" },
   { id: 3, name: "iPhone 16e", price: 48500, image: "/images/iphone16e.jpeg", href: "/iphone/iphone16e" },
   { id: 4, name: "iPhone 15", price: 49500, image: "/images/iphone15.jpeg", href: "/iphone/iphone15" },
+  { id: 5, name: "iPhone 14", price: 49500, image: "/images/iphone15.jpeg", href: "/iphone/iphone14" },
+  { id: 6, name: "iPhone 13", price: 49500, image: "/images/iphone15.jpeg", href: "/iphone/iphone13" },
+  { id: 7, name: "iPhone 12", price: 49500, image: "/images/iphone15.jpeg", href: "/iphone/iphone12" },
+  { id: 8, name: "iPhone 11", price: 49500, image: "/images/iphone15.jpeg", href: "/iphone/iphone11" },
 ];
