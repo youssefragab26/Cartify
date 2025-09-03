57c0f5726d43234b6caa3b9fb1dfe754642cff53
Youssefragab26 <youssef26ragab@gmail.com>
2025-09-03 21:53:17 +0300

    data update

--- src/app/data/airpods.ts
+++ src/app/data/airpods.ts
@@ -3,5 +3,8 @@ export const airpods = [
   { id: 1, name: "AirPods Pro (2nd Gen)", price: 13900, image: "images/airpodspro2.jpeg" },
   { id: 2, name: "AirPods (4rd Gen, ANC)", price: 11900, image: "images/airpods4anc.jpeg" },
   { id: 3, name: "AirPods (4rd Gen)", price: 9900, image: "images/airpods4.jpeg" },
-  { id: 4, name: "AirPods Max", price: 33300, image: "images/airpodsmax.jpeg" },
+  { id: 4, name: "AirPods (3rd Gen)", price: 7700, image: "images/airpods3.jpeg" },
+  { id: 5, name: "AirPods (2nd Gen)", price: 5500, image: "images/airpods2.jpeg" },
+  { id: 6, name: "AirPods (1rd Gen)", price: 5500, image: "images/airpods1.jpeg" },
+  { id: 7, name: "AirPods Max", price: 33300, image: "images/airpodsmax.jpeg" },
 ];
